4130ff5a81ddb7da9df7a89ee573c680b519e002
mansgrundberg <mans.grundberg@gmail.com>
2020-10-22 22:02:19 +0200

    Distance now in cm

--- distance.py
+++ distance.py
@@ -10,12 +10,14 @@ def get_distances(frame, predictions, text):
         scale = math.sqrt(predictions[i].box.area)/math.sqrt(length*width)
         for j in range(i+1, len(predictions)):
             dist = predictions[i].box.compute_distance(predictions[j].box)/scale
+            euclidean_distance = predictions[i].box.compute_distance(predictions[j].box)
 
             # Draw some unnecessary lines between faces
             color = (66,245,93)
             cv.line(frame, (int(predictions[i].box.center[0]), int(predictions[i].box.center[1])), 
             (int(predictions[j].box.center[0]), int(predictions[j].box.center[1])), color, 6)
 
-            if dist > 0.0:
-                text.append("Distance between face " + str(i+1) + " and " + str(j+1) + ": {:.2f} " + " cm".format(dist))
+            text.append("Distance in cm between face " + str(i+1) + " and " + str(j+1) + ": {:.2f} cm".format(dist))
+            text.append("Euclidean distance between face " + str(i+1) + " and " + str(j+1) + ": {:.2f}".format(euclidean_distance))
+            
     return (frame, text)
